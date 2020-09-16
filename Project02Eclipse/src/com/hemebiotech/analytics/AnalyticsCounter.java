4e970ea77b80d0c521753369abb976b3208672ab
aalshoaibi <69863696+aalshoaibi@users.noreply.github.com>
2020-09-16 17:15:17 +0100

    Update AnalyticsCounter.java
    A calss that reads the symtomps from a list and put it into a map and write the result into a file

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,40 +4,75 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+	/**
+	 *  Create a map to use it to get
+	 * the symptoms and count the occurrences.
+	 * assign it to a new Treemap which will allow us to sort the symptoms alphabetically.
+	 * */
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+	Map<String, Integer> countSymptoms = new TreeMap<>();
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+
+	/**
+	 * a method to get the symptoms from
+	 * list and count the  number of occurrences.
+	 * @param symptoms input list
+	 */
+
+
+
+	public void symptomsFromReader(List<String> symptoms) {
+		for (String symptom : symptoms)
+			if (countSymptoms.containsKey(symptom))
+				countSymptoms.put(symptom, countSymptoms.get(symptom) + 1);
+			else
+				countSymptoms.put(symptom, 1);
+
+
+
+
 	}
+
+	/**
+	 * A method to write the map into a file.
+	 *
+	 * @param file file path
+	 */
+	public void write (String file){
+
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			for (Map.Entry<String, Integer> into : countSymptoms.entrySet()) {
+				writer.write(into.getKey() + " = " + into.getValue().toString());
+				writer.newLine();
+
+			}
+			writer.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * A method to use ReadSymptomFromFile class to read files.
+	 *
+	 * @param filepath the path of the file we want to read.
+	 * @return return method Getsymptoms from the class ReadSymptomsFromfile.
+	 */
+	public List<String> readfile(String filepath) {
+		return new ReadSymptomDataFromFile(filepath).GetSymptoms();
+	}
+
+
+
 }
+
